85c0244d7bc5c32a0f207c24bf7f956f528d714e
Marcos Paulo R. Ribeiro <marcos.ribeiro@compasso.com.br>
2021-12-28 11:21:10 -0300

    Adding person class constructor

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,7 @@
 // creating class in JS for object orientation programing
-function Person(){
-    var name;
-    var age;
-    var gender;
-    var phoneNumber;
+function Person(_name, _age){
+    var name = _name;
+    var age = _age;
 
     this.infoMesg = function(name){
         console.log('Info ...\nname:' + name);
@@ -16,9 +14,16 @@ function Person(){
     this.setName = function(_name){
         name = _name
     }
+
+    this.getAge = function(){
+        return age
+    }
+
+    this.setAge = function(_age){
+        age = _age
+    }
 }
 
-var personOne = new Person();
-personOne.setName('Marcos')
+var personOne = new Person("Marcos", 21);
 personOne.infoMesg(personOne.getName());
 console.log(personOne)
